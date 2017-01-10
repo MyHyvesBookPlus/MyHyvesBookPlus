ac369b57056c3ebb561db532802eb5219caa1533
Marijn Jansen <marijn@jansen.onl>
2017-01-10 17:06:52 +0100

    Almost a chatpage!

---
+++ website/views/chat.php
@@ -0,0 +1,20 @@
+<div class="content">
+    <div class="chat">
+        <div class="chat-history">
+            <div class="chat-message-self">Hi!</div><br>
+            <div class="chat-message-other">Hi!</div><br>
+            <div class="chat-message-self">Hi!</div><br>
+
+        </div>
+        <form class="chat-field" method="post">
+            <input type="text"
+                   name="message"
+                   placeholder="Typ een chatbericht om te verzenden"
+                   required
+            >
+            <input type="submit"
+                   value="Verstuur"
+            >
+        </form>
+    </div>
+</div>
