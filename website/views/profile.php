fffc418f4c90e514e756e7bd7e69258e6e35b4eb
Kevin Nobel <kevlebon@gmailcom>
2017-01-12 11:38:56 +0100

    Changed style of windows, we now use the platform class.

--- website/views/profile.php
+++ website/views/profile.php
@@ -1,5 +1,5 @@
 <div class="content">
-    <div class="profile-box">
+    <div class="profile-box platform">
         <img class="left profile-picture" src="http://i.imgur.com/afjEUx2.jpg">
         <div class="profile-button">
             <p><img src="../website/img/add-friend.png"> Als vriend toevoegen</p>
@@ -8,7 +8,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Nunc aliquam neque turpis, id porta quam iaculis id. Sed suscipit, nisl a fermentum congue, nunc augue finibus lectus, id varius nunc purus nec dolor. Integer laoreet tellus sit amet sapien auctor congue. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida. Sed quis fermentum odio, sed dictum nulla. Donec aliquam rutrum orci cursus tempus. Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
     </div>
 
-    <div class="item-box left">
+    <div class="item-box left platform">
         <h2>Vrienden</h2>
         <p>
             <a href="#" data-title="[gebruikersnaam]"><img class="profile-picture" src="http://i.imgur.com/afjEUx2.jpg" alt="[gebruikersnaam]'s profielfoto"></a>
@@ -20,7 +20,7 @@
         </p>
     </div>
 
-    <div class="item-box right">
+    <div class="item-box right platform">
         <h2>Groepen</h2>
         <p>
             <a href="#" data-title="[groepsnaam]"><img class="group-picture" src="http://i.imgur.com/ztYhYro.png" alt="[groepsnaam]'s logo"></a>
@@ -33,7 +33,7 @@
     </div>
 
     <div class="posts">
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida. Sed quis fermentum odio, sed dictum nulla. Donec aliquam rutrum orci cursus tempus. Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
@@ -41,7 +41,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Glitterzz :3</h2>
                 <img src="https://s-media-cache-ak0.pinimg.com/originals/5d/ab/86/5dab8634abeded269f4c456b3b7a6a90.gif" alt="Foto van [gebruikersnaam]">
@@ -49,7 +49,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Nunc aliquam neque turpis, id porta quam iaculis id. Sed suscipit, nisl a fermentum congue, nunc augue finibus lectus, id varius nunc purus nec dolor. Integer laoreet tellus sit amet sapien auctor congue. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida. Sed quis fermentum odio, sed dictum nulla. Donec aliquam rutrum orci cursus tempus. Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
@@ -57,7 +57,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>woooow</h2>
                 <img src="http://31.media.tumblr.com/5227ca92d5d028bae64060d3f32315c2/tumblr_myt7mjJahV1qimzy6o3_250.gif" alt="Foto van [gebruikersnaam]">
@@ -65,7 +65,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>0.0</h2>
                 <img src="https://68.media.tumblr.com/7b2da43b98c43016261831c240381fba/tumblr_o6ol996fHJ1tcu68no1_400.gif" alt="Foto van [gebruikersnaam]">
@@ -73,7 +73,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Nunc aliquam neque turpis, id porta quam iaculis id. Sed suscipit, nisl a fermentum congue, nunc augue finibus lectus, id varius nunc purus nec dolor. Integer laoreet tellus sit amet sapien auctor congue. Sed quis fermentum odio, sed dictum nulla. Donec aliquam rutrum orci cursus tempus. Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
@@ -81,7 +81,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Nunc aliquam neque turpis, id porta quam iaculis id. Sed suscipit, nisl a fermentum congue, nunc augue finibus lectus, id varius nunc purus nec dolor. Integer laoreet tellus sit amet sapien auctor congue. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida. Sed quis fermentum odio, sed dictum nulla.</p>
@@ -89,7 +89,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Nunc aliquam neque turpis, id porta quam iaculis id. Sed suscipit, nisl a fermentum congue, nunc augue finibus lectus, id varius nunc purus nec dolor. Integer laoreet tellus sit amet sapien auctor congue. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida. Sed quis fermentum odio, sed dictum nulla. Donec aliquam rutrum orci cursus tempus. Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
@@ -97,7 +97,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum turpis quam, eu ultrices sapien hendrerit tincidunt. Nunc aliquam neque turpis, id porta quam iaculis id. Sed suscipit, nisl a fermentum congue, nunc augue finibus lectus, id varius nunc purus nec dolor. Integer laoreet tellus sit amet sapien auctor congue. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida.  Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
@@ -105,7 +105,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Tekst</h2>
                 <p>Integer laoreet tellus sit amet sapien auctor congue. Mauris laoreet eu elit vel rhoncus. Nam et tortor arcu. Maecenas sit amet leo quis tellus varius gravida. Sed quis fermentum odio, sed dictum nulla. Donec aliquam rutrum orci cursus tempus. Quisque sit amet ipsum eget velit aliquam facilisis ultricies quis ligula. Nunc nisi lacus, luctus non bibendum quis, sagittis sit amet odio.</p>
@@ -113,7 +113,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Afbeelding</h2>
                 <img src="http://i.imgur.com/tr9amK8.png" alt="Foto van [gebruikersnaam]">
@@ -121,7 +121,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Post</h2>
                 <video width="250" controls>
@@ -131,7 +131,7 @@
             </div>
         </div>
 
-        <div class="post-box">
+        <div class="post-box platform">
             <div class="post">
                 <h2>Post</h2>
                 <p class="post-date">4 Januari geplaatst</p>
