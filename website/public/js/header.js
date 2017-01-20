173ef3e50c2feceb27e715fddbfc33aac08ff5db
Lars van Hijfte <larsvanhijfte@hotmail.com>
2017-01-20 16:17:56 +0100

    Added a notification bar

--- website/public/js/header.js
+++ website/public/js/header.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+    // Hide notification center.
     $("#profile-menu-popup").hide();
+
+    // $("#own-profile-picture").click(function() {
+    //     $("#profile-menu-popup").toggle();
+    //     $("#profile-hello-popup").toggle();
+    // });
+
     $("#own-profile-picture").click(function() {
-        $("#profile-menu-popup").toggle();
-        $("#profile-hello-popup").toggle();
+        if($("#notification-center").css('right') == "-256px") {
+            // $(".content").animate({
+            //     marginRight: "256px"
+            // }, 500);
+            $("#notification-center").animate({
+                right: "0px"
+            }, 500);
+        } else {
+            // $(".content").animate({
+            //     marginRight: "0px"
+            // }, 500);
+            $("#notification-center").animate({
+                right: "-256px"
+            }, 500);
+        }
     });
 });
--- website/views/header.php
+++ website/views/header.php
@@ -19,11 +19,6 @@ $userinfo = getHeaderInfo();
         </form>
     </div>
     <div class="right profile-menu">
-        <div id="profile-menu-popup">
-            <a href="logout.php"><span style="color: red;" class="fa fa-sign-out" data-title="Uitloggen"></span></a> |
-            <a href="settings.php"><span style="color: blue;" class="fa fa-cog" data-title="Instellingen"></span></a> |
-            <a href="profile.php"><span style="color: green;" class="fa fa-user" data-title="Profiel"></span></a>
-        </div>
         <div id="profile-hello-popup">
             <div id="hello-loop">
                 Hallo
@@ -32,5 +27,7 @@ $userinfo = getHeaderInfo();
         </div>
         <img id="own-profile-picture" class="profile-picture" src="<?=$userinfo["profilepicture"]?>"/>
     </div>
-    <a href="chat.php"><div class="right fa fa-comments-o" id="open-chat" data-title="Prive chats"></div></a>
+<!--    <i class="right fa fa-bell-o" id="open-notifications" data-title="Prive chats"></i>-->
 </header>
+<?php include("notification-center.php"); ?>
+
