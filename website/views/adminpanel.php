6418b2b679fdaee5f70da8dddf6321989ea5ea5d
Hendrik <hendrik.huang@student.uva.nl>
2017-01-20 13:14:28 +0100

    move js to /js

--- website/views/adminpanel.php
+++ website/views/adminpanel.php
@@ -3,53 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Admin Panel</title>
-    <script type="text/javascript">
-    window.onload = function() {
-        changeFilter();
-    };
-
-    function checkAll(allbox) {
-        var checkboxes = document.getElementsByClassName('checkbox-list');
-
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].type == 'checkbox') {
-                checkboxes[i].checked = allbox.checked;
-            }
-        }
-    }
-
-    function checkCheckAll(allbox) {
-        var checkboxes = document.getElementsByClassName('checkbox-list');
-        var checked = true;
-
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].type == 'checkbox') {
-                if (checkboxes[i].checked == false) {
-                    checked = false;
-                    break;
-                }
-            }
-        }
-        allbox.checked = checked;
-    }
-
-    function changeFilter() {
-        if (document.getElementById('group').checked) {
-            document.getElementById('admin-filter').style.display = 'none';
-            document.getElementById('admin-groupfilter').style.display = 'inline-block';
-
-            document.getElementById('admin-batchactions').style.display = 'none';
-            document.getElementById('admin-groupbatchactions').style.display = 'inline-block';
-        } else {
-            document.getElementById('admin-filter').style.display = 'inline-block';
-            document.getElementById('admin-groupfilter').style.display = 'none';
-
-            document.getElementById('admin-batchactions').style.display = 'inline-block';
-            document.getElementById('admin-groupbatchactions').style.display = 'none';
-        }
-    }
-
-    </script>
+    <script src="../public/js/admin.js" charset="utf-8"></script>
     <?php
         include_once("../queries/user.php");
         include_once("../queries/group_page.php");
@@ -228,7 +182,7 @@ function test_input($data) {
                         if ($pagetype == "user") {
                             $pages = countSomeUsersByStatus($search, $status);
                         } else {
-                            $pages = countSomeGroupsByStatus($search, $status);
+                            $pages = countSomeGroupsByStatus($search, $groupstatus);
                         }
                         $countresults = $pages->fetchColumn();
                         $mincount = min($listm, $countresults);
