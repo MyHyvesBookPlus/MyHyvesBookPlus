5c1208460f37a1552405d8b95f6cffbc0ede0da2
Lars van Hijfte <larsvanhijfte@hotmail.com>
2017-01-26 16:12:50 +0100

    Added direct button on the profile to chat with someone

--- website/public/js/friendButtons.js
+++ website/public/js/friendButtons.js
@@ -4,12 +4,14 @@ function placeFriendButtons() {
             friendshipStatus = data;
             $buttonContainer = $("div.friend-button-container");
             $buttonContainer.children().remove();
+            $("#start-profile-chat-form").hide();
             if (friendshipStatus == -1) {
                 return;
             } else if(friendshipStatus == 0) {
                 $buttonContainer.append($("<button class=\"green friend-button\" value=\"request\"><i class=\"fa fa-handshake-o\"></i> Bevriend</button>"));
             } else if(friendshipStatus == 1) {
                 $buttonContainer.append($("<button class=\"red friend-button\" value=\"delete\"><i class=\"fa fa-times\"></i> Verwijder</button>"));
+                $("#start-profile-chat-form").show();
             } else if(friendshipStatus == 2) {
                 $buttonContainer.append($("<button class=\"red friend-button\" value=\"delete\"><i class=\"fa fa-times\"></i> Trek verzoek in</button>"));
             } else if(friendshipStatus == 3) {
