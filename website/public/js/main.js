cbff973b631126093c0b8dad90dcb3513eca38ba
Lars van Hijfte <larsvanhijfte@hotmail.com>
2017-01-27 11:15:33 +0100

    Save manu state with cookie

--- website/public/js/main.js
+++ website/public/js/main.js
@@ -1,3 +1,14 @@
+function getCookie(key) {
+    cookies = document.cookie.split("; ");
+    for (var i in cookies) {
+        cookie = cookies[i].split("=");
+        if (cookie[0] == key) {
+            return cookie[1];
+        }
+    }
+    return false;
+}
+
 function editFriendship(userID, value) {
     $.post("API/editFriendship.php", { usr: userID, action: value })
     .done(function() {
