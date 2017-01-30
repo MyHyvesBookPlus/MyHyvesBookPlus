20b951cad27bff398049065ba8970f4aaee6b666
Marijn Jansen <marijn@jansen.onl>
2017-01-30 14:30:36 +0100

    404 page

---
+++ website/public/error/404.php
@@ -0,0 +1,9 @@
+<?php
+require_once "../../views/messagepage.php";
+
+messagePage("
+<div class='error-page'>
+    <h1>404</h1>
+    <h4>Wrong link...</h4><br />
+    <img height='486' width='204' src='../img/zelda.png'>
+</div>");
--- website/views/messagepage.php
+++ website/views/messagepage.php
@@ -5,14 +5,14 @@ function messagePage(string $content) {
     <html>
     <head>
         <style>
-            @import url(styles/main.css);
-            @import url(styles/settings.css);
-            @import url(styles/resetpassword.css);
+            @import url(../styles/main.css);
+            @import url(../styles/settings.css);
+            @import url(../styles/message-page.css);
         </style>
     </head>
     <body>
     <div class='password-change'>
-        <div class='top-logo'><a href='login.php'><img src='img/top-logo.png' alt='MyHyvesbook+'/></a></div>
+        <div class='top-logo'><a href='../login.php'><img src='../img/top-logo.png' alt='MyHyvesbook+'/></a></div>
         <div class='item-box platform'>$content</div>
     </div>
     </body>
