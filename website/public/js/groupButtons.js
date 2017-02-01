69b0b24176b5cc058a5e5b6fb920b645b5da697e
K. Nobel <kevlebon@gmailcom>
2017-02-01 12:26:05 +0100

    Fixed group add/leave buttons

---
+++ website/public/js/groupButtons.js
@@ -0,0 +1,33 @@
+function placeGroupButtons() {
+    $.post("API/getGrouprole.php", { grp: groupID })
+        .done(function(data) {
+            var $buttonContainer = $("div.group-button-container");
+
+            if(data == 'none') {
+                $buttonContainer.append(
+                    "<button class='green group-button' value='request'>" +
+                    "<i class='fa fa-plus'></i> Voeg toe" +
+                    "</button>");
+            } else if(data == 'request') {
+                $buttonContainer.append(
+                    "<button class='red group-button' value='none'>" +
+                    "<i class='fa fa-times'></i> Trek verzoek in" +
+                    "</button>");
+            } else {
+                $buttonContainer.append(
+                    "<button class='red group-button' value='none'>" +
+                    "<i class='fa fa-times'></i> Verlaat groep" +
+                    "</button>");
+            }
+
+            $buttonContainer.children().click(function() {
+                $.post("API/editMembership.php", { grp: groupID, role: this.value })
+                    .done(function() {
+                        $buttonContainer.children().remove();
+                        placeGroupButtons();
+                    }).fail(function() {
+                });
+            });
+
+    });
+}
