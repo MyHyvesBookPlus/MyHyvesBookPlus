74145d5d1c808b5c37a0646d27804aaa6e9c386c
Marijn Jansen <marijn@jansen.onl>
2017-02-02 19:43:57 +0100

    Updated buttons on groups

--- website/public/js/groupButtons.js
+++ website/public/js/groupButtons.js
@@ -3,31 +3,41 @@ function placeGroupButtons() {
         .done(function(data) {
             var $buttonContainer = $("div.group-button-container");
 
-            if(data == 'none') {
+            if (data == 'none') {
                 $buttonContainer.append(
-                    "<button class='green group-button' value='request'>" +
+                    "<button class='green group-button group-button-fixed' value='request'>" +
                     "<i class='fa fa-plus'></i> Voeg toe" +
                     "</button>");
-            } else if(data == 'request') {
+            } else if (data == 'request') {
                 $buttonContainer.append(
-                    "<button class='red group-button' value='none'>" +
+                    "<button class='red group-button group-button-fixed' value='none'>" +
                     "<i class='fa fa-times'></i> Trek verzoek in" +
                     "</button>");
+            } else if (data == 'admin') {
+                $buttonContainer.append(
+                    "<button class='group-button group-button-fancy' value='admin'>" +
+                        "<span>Instellingen</span><i class='fa fa-cogs'></i>" +
+                        "</button>"
+                );
             } else {
                 $buttonContainer.append(
-                    "<button class='red group-button' value='none'>" +
-                    "<i class='fa fa-times'></i> Verlaat groep" +
+                    "<button class='red group-button group-button-fancy' value='none'>" +
+                    "<span>Verlaat groep</span><i class='fa fa-sign-out'></i>" +
                     "</button>");
             }
 
             $buttonContainer.children().click(function() {
-                $.post("API/editMembership.php", { grp: groupID, role: this.value })
-                    .done(function() {
-                        $buttonContainer.children().remove();
-                        placeGroupButtons();
-                        updateMenus();
-                    }).fail(function() {
-                });
+                if (this.value == 'admin') {
+                    window.location.href='groupAdmin.php?groupID=' + groupID;
+                } else {
+                    $.post("API/editMembership.php", {grp: groupID, role: this.value})
+                        .done(function () {
+                            $buttonContainer.children().remove();
+                            placeGroupButtons();
+                            updateMenus();
+                        }).fail(function () {
+                    });
+                }
             });
 
     });
